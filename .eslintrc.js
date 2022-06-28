6cc23f011257cf80e186b362320a76aadea74d03
ys-logical0 <ys.logical0@gmail.com>
2022-06-29 03:09:32 +0900

    fix project setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["standard", "prettier"],
+  extends: ["eslint:recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
